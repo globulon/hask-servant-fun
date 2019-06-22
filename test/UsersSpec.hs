e0bc72475e767d59c3ecff2fb7edf4fa136ec0ea
Marc-Daniel Ortega <globulon@gmail.com>
2019-06-22 21:40:35 +0100

    Fix tests

--- test/UsersSpec.hs
+++ test/UsersSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module UsersSpec where
 
 import Domain(User(..))
@@ -10,7 +11,7 @@ import Algebras
 import Interpreters
 import Control.Monad.Trans.Reader (ReaderT(..), mapReaderT, ask)
 import Control.Monad.IO.Class (liftIO, MonadIO)
-
+import Control.Monad.Trans.Except
 
 spec :: Spec
 spec = do
@@ -22,32 +23,32 @@ spec_add_user :: Spec
 spec_add_user =
   describe "added user" $
   it "should be found" $
-    let u = do
-            _ <- addUser usr
-            userByName (name usr)
-    in makeEnv >>= runReaderT u >>= (`shouldBe` Just usr)
+    let u :: UserIO User = do
+          _ <- addUser usr
+          userByName (name usr)
+    in makeEnv >>= runExceptT . runReaderT u >>= (`shouldBe` Right usr)
 
 spec_delete_user :: Spec
 spec_delete_user =
   describe "delete user" $
   it "should delete user" $
-    let u = do
+    let u :: UserIO User = do
             n <- fmap name (return usr)
             _ <- addUser usr
             _ <- dropUser n
             userByName n
-    in makeEnv >>= runReaderT u >>= (`shouldBe` Nothing)
+    in makeEnv >>= runExceptT . runReaderT u >>= (`shouldBe` Left (UserNotFound "omd"))
 
 spec_all_users :: Spec
 spec_all_users =
   describe "all users" $ do
     it "should send no users" $
-      makeEnv >>= runReaderT allUsers >>= (`shouldBe` [])
+      makeEnv >>= runExceptT . runReaderT (allUsers :: UserIO [User]) >>= (`shouldBe` Right [])
     it "should send a created user" $
-      let us = do
+      let us :: UserIO [User] = do
                _ <- addUser usr
                allUsers
-      in makeEnv >>= runReaderT us >>= (`shouldBe` [usr])
+      in makeEnv >>= runExceptT . runReaderT us >>= (`shouldBe` Right [usr])
 
 usr :: User
 usr = User "omd" "globulon@gmail.com" (fromGregorian 1971 8 28)
